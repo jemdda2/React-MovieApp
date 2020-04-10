64a1e2f73983521ce62fb5243af7b81b76130405
Jisoon <siriusda2@naver.com>
2020-04-10 13:04:54 +0900

    GridCards作成

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,6 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+export const API_KEY = '3f9180c9d1f8f17dc4d88e3351cbaad2';
+export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
+export const IMAGE_SIZE = 'w1280';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
+
+// import { API_URL } from '../../Config';
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+
+            console.log(response.results[0]);
+            
+            setMovies([...response.results])
+            setMainMovieImage(response.results[0])
+        })
+
+    }, [])
+
     return (
-        <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-        </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div style={{ width: '100%', margin: '0' }}>
+
+            {/* Main Image */}
+            {MainMovieImage &&
+                <MainImage 
+                    image={`${IMAGE_URL}w1280${MainMovieImage.backdrop_path}`} 
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+
+
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <h2>Movies by latest</h2>
+                <hr />
+
+                {/* Movie Grid Cards */}
+
+                <Row gutter={[ 16, 16 ]}>
+                    {Movies && Movies.map((movie, index) => (
+                            <React.Fragment key={index}>
+                                <GridCards
+                                    image={movie.poster_path ?
+                                        `${IMAGE_URL}w500${movie.poster_path}` : null}
+                                    movieId={movie.id}
+                                    movieName={movie.original_title}
+                                />
+                            </React.Fragment>
+                        ))}
+                    
+                </Row>
+
+                
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button> Load More</button>
+            </div>
+
+        </div>            
     )
 }
 
