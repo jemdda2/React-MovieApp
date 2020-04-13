9f8d08a4fbb72bec2572a2a5afae5b83016156c8
Jisoon <siriusda2@naver.com>
2020-04-13 23:00:49 +0900

    FavoritePage作成

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -50,7 +50,24 @@ router.post('/addToFavorite', (req, res) =>{
         if(err) return res.status(400).send(err);
         return res.status(200).json( { success: true} )
     });
+})
 
+router.post('/getFavoredMovie', (req, res) =>{
+
+    Favorite.find({ 'userFrom': req.body.userFrom })
+        .exec((err, favorites) => {
+            if(err) return res.status(400).send(err);
+            return res.status(200).json({ success: true, favorites });
+        });
+})
+
+router.post('/removeFromFavorite', (req, res) =>{
+
+    Favorite.findOneAndDelete({ movieId: req.body.movieId, userFrom: req.body.userFrom })
+        .exec((err, result) => {
+            if(err) return res.status(400).send(err);
+            return res.status(200).json({ success: true, result })
+        })
 })
 
 module.exports = router;
